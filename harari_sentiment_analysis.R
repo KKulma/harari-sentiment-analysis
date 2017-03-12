9db9eb799bf60837009163d2d29bb5689b9639cc
Kasia <katarzyna.kulma@gmail.com>
2017-03-12 15:26:05 +0000

    text preprocessing, product, page and tokens functions

--- harari_sentiment_analysis.R
+++ harari_sentiment_analysis.R
@@ -2,26 +2,22 @@
 rm(list=ls())
 ls()
 
-install.packages("pacman")
-pacman::p_load(XML, dplyr, stringr, rvest, audio, xml2)
+#install.packages("pacman")
+pacman::p_load(XML, dplyr, stringr, rvest, audio, xml2, purrr, tidytext)
 update.packages()
 
-#Remove all white space
-trim <- function (x) gsub("^\\s+|\\s+$", "", x)
 
 sapiens_code = "1846558239"
 deus_ex_code = "1910701874"
 
 function_product <- function(prod_code){
-  trim <- function (x) gsub("^\\s+|\\s+$", "", x)
-  
   url <- paste0("https://www.amazon.co.uk/dp/",prod_code)
   doc <- xml2::read_html(url)
   prod <- html_nodes(doc,"#productTitle") %>% html_text() %>%
-    gsub("\n","",.) %>% trim()
+    gsub("\n","",.) %>%
+    gsub("^\\s+|\\s+$", "", .) #Remove all white space
   prod
 }
-
 
 sapiens <- function_product(sapiens_code)
 deus_ex <- function_product(deus_ex_code)
@@ -34,21 +30,46 @@ deus_ex
 #Source funtion to Parse Amazon html pages for data
 source("https://raw.githubusercontent.com/rjsaito/Just-R-Things/master/Text%20Mining/amazonscraper.R")
 
+pages <- 13
 
-
-
-### sugestion
-
-library("purrr")
-library("dplyr")
-pages <- 50
-
-function_page <- function(page){
-  url2 <- paste0("http://www.amazon.co.uk/pro...",prod_code,"/?pageNumber=", page_num)
+function_page <- function(page_num, prod_code){
+  url2 <- paste0("http://www.amazon.co.uk/product-reviews/",prod_code,"/?pageNumber=", page_num)
   doc2 <- read_html(url2)
   
   reviews <- amazon_scraper(doc2, reviewer = F, delay = 2)
   reviews
 }
 
-reviews_all <- map(1:pages, function_page) %>% bind_rows()
+sapiens_reviews <- map2(1:pages, sapiens_code, function_page) %>% bind_rows()
+str(sapiens_reviews)
+
+deusex_reviews <- map2(1:pages, deus_ex_code, function_page) %>% bind_rows()
+str(deusex_reviews)
+
+sapiens_reviews$comments <- gsub("\\.", "\\. ", sapiens_reviews$comments)
+deusex_reviews$comments <- gsub("\\.", "\\. ", deusex_reviews$comments)
+
+### sentiment analysis ####
+
+tokens_function <- function(df){
+  df_words <- df %>% 
+  select(comments, format, stars, helpful) %>% 
+  unnest_tokens(word, comments)
+  
+  data("stop_words")
+  
+  df_words <- df_words %>%
+    anti_join(stop_words)
+  
+  df_words
+}
+
+sapiens_words <- tokens_function(sapiens_reviews)
+deusex_words <- tokens_function(deusex_reviews)
+
+head(sapiens_words)
+tail(sapiens_words)
+
+head(deusex_words)
+tail(deusex_words)
+  
